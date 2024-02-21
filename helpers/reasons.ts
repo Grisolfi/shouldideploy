53867ad716425daae41c5419e346a756a183c6c7
Thiago Grisolfi <thiago.grisolfi@grupoboticario.com.br>
2024-02-21 01:10:17 -0300

    Update reasons.ts

--- helpers/reasons.ts
+++ helpers/reasons.ts
@@ -12,8 +12,7 @@ export const REASONS_TO_DEPLOY = [
   'Make me proud',
   'Break a leg!',
   'This Is the Way',
-  'Strike First, Strike Hard, No Mercy!',
-  'Se quiser sim, mano.'
+  'Strike First, Strike Hard, No Mercy!'
 ]
 
 export const REASONS_TO_NOT_DEPLOY = [
